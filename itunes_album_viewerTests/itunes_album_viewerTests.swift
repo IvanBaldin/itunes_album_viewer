cd9b5aad75ec09522ef957891367eae654c8188a
Ivan Baldin <evan.baldin@gmail.com>
2021-02-06 23:40:48 +0300

    deleted unused test

--- itunes_album_viewerTests/itunes_album_viewerTests.swift
+++ itunes_album_viewerTests/itunes_album_viewerTests.swift
@@ -53,12 +53,4 @@ class itunes_album_viewerTests: XCTestCase {
             XCTAssertEqual(expectedSongsCount, recievedSongsCount)
         }
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
